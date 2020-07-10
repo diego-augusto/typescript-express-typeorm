39d8ef85920daf367ac3105a4e47de31d2739744
Diego Augusto <daugustomoura@gmail.com>
2020-07-10 19:01:48 -0300

    refactor: remove auth controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,3 @@
-export * as AuthController from './AuthController'
 export * as ProductController from './ProductController'
 export * as StoreController from './StoreController'
 export * as UserController from './UserController'
